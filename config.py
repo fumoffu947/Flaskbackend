56d6962b65752d183d23df98acc66d876904cb99
Philip Johansson <phijo967@student.liu.se>
2015-04-29 09:32:41 +0200

    r

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import os
 
+basedir = os.environ['OPENSHIFT_DATA_DIR']
+
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['DATABASE_PATH'] = os.path.join(basedir,'app_databese.db')
-
-basedir = os.environ['OPENSHIFT_DATA_DIR']
