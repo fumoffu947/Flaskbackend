95e2afe4873e22b358a576782154bf764dbcd14f
Philip Johansson <phijo967@student.liu.se>
2015-05-12 09:30:32 +0200

    r

--- config.py
+++ config.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['DATABASE_PATH'] = os.path.join(basedir,'app_database.db')
 
-troll = 1
 
 def init_db():
     con = sqlite3.connect(app.config['DATABASE_PATH'])
